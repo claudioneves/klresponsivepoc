b7954d9608bc671cab3178d663e6a8eb863086af
Claudio Neves <claudio.neves@bbc.co.uk>
2012-12-14 10:27:14 +0000

    taks 1, 4, 5, 8, 9, 10, 12

--- main.rb
+++ main.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Application < Sinatra::Base
   get '/' do
-    erb :index
+    @page_type = 'kl'
+    erb :index    
+  end
+  
+  get '/gel' do
+    @page_type = 'gel'
+    erb :index    
   end
 end
 
